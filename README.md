17443b8b93fce12b8ab6f7211a27aeed62b1ff49
tanislf <tanialopezmoretz@gmail.com>
2025-06-27 17:09:32 -0600

    new4

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Tripleten web_project_around
+
+Github Repository:
+https://github.com/tanislf/web_project_around.git
+
+Githug Pages: 
+https://tanislf.github.io/web_project_around/
--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,34 @@
 //variables a utilizar
-let saveButton = document.getElementById("save");
-let nameUser = document.getElementById("name");
-let jobInfo = document.getElementById("info");
+let form = document.getElementById("form");
+let nameUser = document.querySelector(".profile__info-name");
+let jobInfo = document.querySelector(".profile__info-description");
 let nameInformation = document.getElementById("editName");
 let jobInformation = document.getElementById("editJob");
+let saveButton = document.getElementById("save");
+let editButton = document.getElementById("editButton");
+let closeButton = document.getElementById("close");
 
 //función para mostrar el formulario y la información escrita
-function ShowHide() {
-  let form = (document.getElementById("form").style.display = "flex");
-  nameInformation.value = nameUser.innerText;
-  jobInformation.value = jobInfo.innerText;
+function openForm(evt) {
+  document.getElementById("form").style.display = "flex";
+  nameInformation.value = nameUser.textContent;
+  jobInformation.value = jobInfo.textContent;
 }
 
 //función para editar los nombres en el formulario
-function saveData() {
-  let nameInformation = document.getElementById("editName").value;
-  let jobInformation = document.getElementById("editJob").value;
-
-  nameUser.innerText = nameInformation;
-  jobInfo.innerText = jobInformation;
+function saveData(evt) {
+  evt.preventDefault();
+  nameUser.textContent = nameInformation.value;
+  jobInfo.textContent = jobInformation.value;
+  close();
 }
+
+//función para cerrar el formulario
+function closeForm(evt) {
+  document.getElementById("form").style.display = "none";
+}
+
+//Eventos para llamar a las ficnones
+form.addEventListener("submit", saveData);
+editButton.addEventListener("click", openForm);
+closeButton.addEventListener("click", closeForm);
